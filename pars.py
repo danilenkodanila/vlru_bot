f60c6e1636991c5fc1885d5f7c7bbfc1dfb6121e
Евгений <THEklasterstorm@gmail.com>
2016-07-10 23:34:51 +1000

    Эй парень, хочешь немного питона?
    А может это уж или кобра, кто их разберет?

--- pars.py
+++ pars.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
+import sys
+reload(sys)
+sys.setdefaultencoding('cp866') # устанавливаем кодировку вывода консоли
 from grab import Grab #импортируем граб для работы с парсингом
 
 url = 'https://news.yandex.ru/index.rss'
@@ -11,6 +14,12 @@ g.go(url)
 page = g.doc.select(xpath)
 
 mainString = ""
+count = -2
+
 for element in page:
-    mainString = mainString + element.html()
-mainString = mainString.replace("<title>","").replace("</title>","")
+	count = count + 1
+	if count > 0:
+		mainString = mainString + str(count) + ") " + element.html()
+
+
+mainString = mainString.replace("<title>","").replace("</title>","").replace("  ","")
