f518ae40abdce5efe6901afb9d244332d2f66ebf
danilenkodanila <buliga96@mail.ru>
2016-07-10 15:07:29 +1000

    Выводит все тайтлы

--- pars.py
+++ pars.py
@@ -1,11 +1,16 @@
 from grab import Grab #импортируем граб для работы с парсингом
 
 url = 'https://news.yandex.ru/index.rss'
-xpath = '//description'
+xpath = '//title'
 
 g = Grab()
 g.go(url)
+
+
 page = g.doc.select(xpath)
 
+s = ""
 for element in page:
-    s = element.html()
+    s = s + element.html()
+
+
