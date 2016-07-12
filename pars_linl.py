5a19589b46a85d9b7ee359d519276da0ac305c7b
danilenkodanila <buliga96@mail.ru>
2016-07-13 02:18:59 +1000

    python3 без регистрации и СМС
    conda create -n py3k python=3 anaconda в уютной консоличке введи

--- pars_linl.py
+++ pars_linl.py
@@ -4,4 +4,4 @@ import logging
 g = Grab()
 g.go('https://news.yandex.ru/yandsearch?cl4url=regnum.ru%2Fnews%2Fpolit%2F2156222.html&lr=75&lang=ru&rubric=index')
 
-print(g.xpath_text('//a[@class="link link_theme_normal i-bem link_js_inited"]') 
+print(g.xpath_text('//a[@class="link link_theme_normal i-bem link_js_inited"]')  #чтобы новый коммент грузануть напишу это
