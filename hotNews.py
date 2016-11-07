cfeb730290dee164cfe41b4fc1d0188f0690691a
danilenkodanila <buliga96@mail.ru>
2016-11-07 21:56:45 +1000

    delete #
    Расскомментрировал нужную строку

--- hotNews.py
+++ hotNews.py
@@ -1,6 +1,6 @@
 import re
 import sys
-from bs4 import BeautifulSoup
+from bs4 import 3
 from urllib.request import urlopen
 
 doc = 'http://www.newsvl.ru/'
@@ -30,10 +30,10 @@ for div in data:
 print(link)
 print(title)
 
-# finish.append('1) ' + '<a href="' + link[0] + '">' + title[0]+ '</a>' + '\n'+ '2) ' + '<a href="' + link[1] + '">' +title[1]  + '</a>' + '\n' + '3) ' + '<a href="' + link[2] + '">' + title[2] + '</a>') 
+finish.append('1) ' + '<a href="' + link[0] + '">' + title[0]+ '</a>' + '\n'+ '2) ' + '<a href="' + link[1] + '">' +title[1]  + '</a>' + '\n' + '3) ' + '<a href="' + link[2] + '">' + title[2] + '</a>') 
 print(finish)
 
-finish.append('1) ' + '<a href="https://news.yandex.ru/yandsearch?cl4url=regnum.ru/news/society/2202110.html&lang=ru&from=main_portal&lr=75">Госорганы РФ не смогут требовать от граждан 85 видов справок</a>' + '\n'+ '2) ' + '<a href="https://news.yandex.ru/yandsearch?cl4url=www.kommersant.ru/doc/3135777&lang=ru&from=main_portal&lr=75">Украина предоставит коридор для российских войск в Приднестровье</a>' + '\n' + '3) ' + '<a href="https://news.yandex.ru/yandsearch?cl4url=www.rbc.ru/politics/07/11/2016/58205a429a79479cbd632f95&lang=ru&from=main_portal&lr=75">Саакашвили объявил об уходе в отставку</a>') 
+# finish.append('1) ' + '<a href="https://news.yandex.ru/yandsearch?cl4url=regnum.ru/news/society/2202110.html&lang=ru&from=main_portal&lr=75">Госорганы РФ не смогут требовать от граждан 85 видов справок</a>' + '\n'+ '2) ' + '<a href="https://news.yandex.ru/yandsearch?cl4url=www.kommersant.ru/doc/3135777&lang=ru&from=main_portal&lr=75">Украина предоставит коридор для российских войск в Приднестровье</a>' + '\n' + '3) ' + '<a href="https://news.yandex.ru/yandsearch?cl4url=www.rbc.ru/politics/07/11/2016/58205a429a79479cbd632f95&lang=ru&from=main_portal&lr=75">Саакашвили объявил об уходе в отставку</a>') 
 
 
 
