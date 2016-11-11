cdaf337b5abc21669f7085c4c3322d53ae98b28f
danilenkodanila <buliga96@mail.ru>
2016-11-12 00:33:42 +1000

    go to server

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
-token = '253106879:AAFaqpl-zDyps0KITTnA9MQZv83cjjSTges' #этот токен получаешь, когда регистируешь бота 
+# token = '253106879:AAFaqpl-zDyps0KITTnA9MQZv83cjjSTges' #этот токен получаешь, когда регистируешь бота 
+token = '299082168:AAGZvqlZQbv2cptmBjMdRyrbg930tnkOTFA'
